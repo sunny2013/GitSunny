6eddee4b6adc7b7eabd7450fbd59dc742e5c4a8b
sunny2013 <sunqh_dlmu@sina.com>
2015-01-16 17:04:25 +0800

    My Git Demo!

---
+++ ConMyGit/ConMyGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConMyGit
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Git Test!");
+			Console.ReadKey();
+		}
+	}
+}
